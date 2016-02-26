e6ff15186170da9d545ca944ca68af5226590e6c
jess.lee0124 <jess.lee0124@gmail.com>
2016-02-25 22:15:02 -0500

    Update

---
+++ learn.php
@@ -0,0 +1,26 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "cmln3@naver.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $subject2 = "Form submission";
+    $subject3 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following: " . $subject . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
+    }
+
+  if (headers_sent()) {
+  	die("Redirect failed.");
+  } else {
+  	exit(header("Location: /bulk.html"));
+  }
+?>
